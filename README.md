e5da3f23b0ad0637e382ded8667b97e90f341910
Douglas Dias Leal <douglas_san@hotmail.com>
2025-07-09 02:59:08 +0900

    chore: enhance README with Biome and Ultracite setup instructions, add VSCode settings, and implement Ultracite rules for accessibility in the project

--- README.md
+++ README.md
@@ -23,3 +23,21 @@ Install dependencies:
 ```sh
 pnpm i fastify @fastify/cors fastify-type-provider-zod zod
 ```
+
+Biome:
+```sh
+pnpm i @biomejs/biome -D
+```
+
+- [Ultracite](https://www.ultracite.ai/)
+```sh
+pnpm dlx ultracite init
+```
+
+## Extensions
+
+- [Biome](https://marketplace.cursorapi.com/items?itemName=biomejs.biome)
+- [Custom UI Style](https://marketplace.cursorapi.com/items?itemName=subframe7536.custom-ui-style)
+- [REST Client](https://marketplace.cursorapi.com/items?itemName=humao.rest-client)
+- [Symbols](https://marketplace.cursorapi.com/items?itemName=miguelsolorio.symbols)
+- [Tailwind CSS IntelliSense](https://marketplace.cursorapi.com/items?itemName=bradlc.vscode-tailwindcss)
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,23 @@
-import { fastify } from "fastify"
-import {
-    serializerCompiler, validatorCompiler, type ZodTypeProvider
-} from 'fastify-type-provider-zod'
 import { fastifyCors } from '@fastify/cors'
-import { env } from "./env.ts"
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-    origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173',
 })
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.get('/health', () => {
-    return 'OK'
+  return 'OK'
 })
 
-app.listen({ port: env.PORT }).then(() => {
-    console.log(`Server is running on port ${env.PORT}!`)
-})
+app.listen({ port: env.PORT })
